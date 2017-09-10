19ae6a3414a82dd7787d52548aef81a5e965178f
GregViguier <gregory_v@icloud.com>
2017-09-10 13:12:19 +0200

    Use constant variables

--- dash-to-ifttt.py
+++ dash-to-ifttt.py
@@ -1,14 +1,18 @@
 import requests
 from scapy.all import *
 
+DASH_MAC_ADRESS = '<SET_VALUE_HERE>'
+IFTTT_TRIGGER_NAME = '<SET TRIGGER NAME>'
+IFTTT_KEY = '<SET KEY>'
+
 def record():
-        r = requests.post('https://maker.ifttt.com/trigger/<TRIGGER>/with/key/<YOUR IFTTT MAKER KEY>')
+        r = requests.post('https://maker.ifttt.com/' + IFTTT_TRIGGER_NAME + '/with/key/' + IFTTT_KEY)
         #print r.text
 
 def arp_display(pkt):
   if pkt.haslayer(ARP):
         if pkt[ARP].op == 1: #who-has (request)
-                if pkt[ARP].hwsrc == 'DASH_MAC_ADDRESS':
+                if pkt[ARP].hwsrc == DASH_MAC_ADDRESS:
                                 print "Pushed Button"
                                 record()
         else:
